606fb169c156e3afc2db6586d2100c3cb111c6bc
Lalith Kumar <adhimulamlalith22@gmail.com>
2024-09-30 14:27:36 +0530

    Adding Static Files

--- BackendHolder/settings.py
+++ BackendHolder/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-5(rf0128y0!i%2k3%5rtyk7(60sb02u$a=ct4)pz08mq@qzoag'
 
